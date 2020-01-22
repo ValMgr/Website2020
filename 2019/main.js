f77be38870c0be344198671571119e970450eba4
ValMgz <valou32720@gmail.com>
2020-01-22 19:49:51 +0100

    add diaporama

---
+++ 2019/main.js
@@ -0,0 +1,30 @@
+    var el
+    function defineEl(element){
+        el = element
+    }
+    
+    var index = 1;
+    var i = 1
+    function changeURL(el){
+        while(i == index){
+            i = Math.floor(Math.random() * 8);
+        }
+        
+        index = i
+        el.classList.remove('fadeIn')
+        el.classList.add('fadeOut')
+        setTimeout(function () {
+            el.src = 'IMG/bg/bg-0' + index + '.jpg';
+            console.log("url change")
+            setTimeout(function () {
+                el.classList.add('fadeIn')
+                el.classList.remove('fadeOut')
+            }, 100);
+
+        }, 1000);
+        
+
+    }
+
+
+    setInterval(function() { changeURL(el); }, 10000); // Il faut changer ce nombre en millisecondes pour choisir le temps entre chaque photos
