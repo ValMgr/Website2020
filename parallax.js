11368799bced5cb0f65d7c070c5decafca32a0bb
Valentin Magry <valou32720@gmail.com>
2020-02-01 19:07:22 +0100

    Fix z-index and greyshade

--- parallax.js
+++ parallax.js
@@ -1,19 +1,21 @@
-var currentX = '';
-var currentY = '';
-var movementConstant = .005;
-$(document).mousemove(function(e) {
-  if(currentX == '') currentX = e.pageX;
-  var xdiff = e.pageX - currentX;
-  currentX = e.pageX;
-   if(currentY == '') currentY = e.pageY;
-  var ydiff = e.pageY - currentY;
-  currentY = e.pageY; 
-  $('.parallax div').each(function(i, el) {
-      var movement = (i + 1) * (xdiff * movementConstant);
-	  var movementy = (i + 1) * (ydiff * movementConstant);
-      var newX = $(el).position().left + movement;
-	  var newY = $(el).position().top + movementy;
-      $(el).css('left', newX + 'px');
-	  $(el).css('top', newY + 'px');
+if (window.matchMedia("(min-width: 600px)").matches) {
+  var currentX = '';
+  var currentY = '';
+  var movementConstant = .005;
+  $(document).mousemove(function(e) {
+    if(currentX == '') currentX = e.pageX;
+    var xdiff = e.pageX - currentX;
+    currentX = e.pageX;
+    if(currentY == '') currentY = e.pageY;
+    var ydiff = e.pageY - currentY;
+    currentY = e.pageY; 
+    $('.parallax div').each(function(i, el) {
+        var movement = (i + 1) * (xdiff * movementConstant);
+      var movementy = (i + 1) * (ydiff * movementConstant);
+        var newX = $(el).position().left + movement;
+      var newY = $(el).position().top + movementy;
+        $(el).css('left', newX + 'px');
+      $(el).css('top', newY + 'px');
+    });
   });
-});
+}
