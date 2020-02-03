afbb78a4e438ed5b912ebd7dcefccd267ec96d2c
Valentin Magry <valou32720@gmail.com>
2020-02-03 09:41:16 +0100

    add scroll to element

--- main.js
+++ main.js
@@ -6,23 +6,31 @@
     var index = 1;
     var i = 1
     function changeURL(el){
-        while(i == index){
-            i = Math.floor(Math.random() * 8); // Nbr de photo
-        }
-        
-        index = i
-        el.classList.remove('fadeIn')
-        el.classList.add('fadeOut')
-        setTimeout(function () {
-            el.src = 'IMG/bg/bg-0' + index + '.jpg';
+        if(el != null){
+            while(i == index){
+                i = Math.floor(Math.random() * 8); // Nbr de photo
+            }
+            
+            index = i
+            el.classList.remove('fadeIn')
+            el.classList.add('fadeOut')
             setTimeout(function () {
-                el.classList.remove('fadeOut')
-                el.classList.add('fadeIn')
-            }, 500);
+                el.src = 'IMG/bg/bg-0' + index + '.jpg';
+                setTimeout(function () {
+                    el.classList.remove('fadeOut')
+                    el.classList.add('fadeIn')
+                }, 600);
 
-        }, 1000);
-        
+            }, 1000);
+        }
+    }
 
+    function ScrollTo(player){
+        target = document.getElementById(player);
+        target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'center'
+        });
     }
 
 
